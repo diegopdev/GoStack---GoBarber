0366cf78039272459a0f44685e9a4adea782f059
Diego <playskil@gmail.com>
2019-10-28 10:32:21 -0300

    :rocket: initial commit

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8c4d4c2ecb2c094cf538ee00d10239e1',
+  expiresIn: '7d',
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentsController';
+import ScheduleController from './app/controllers/ScheduleController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
+routes.get('/appointments', AppointmentsController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
